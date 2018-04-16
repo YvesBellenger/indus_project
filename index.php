a055fcb4016a96c5281e9af8dc317626d3709b61
Yves BELLENGER <yvesbellenger@hotmail.com>
2018-04-17 01:39:55 +0200

    fix index comments

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
-
-/** index.php **/ 
-
+/**
+ * MyIndex
+ * 
+ * PHP Version 7.1
+ *
+ * @category Test
+ * @package  MyApp
+ * @author   Jeremy Moreau <yvesbellenger@hotmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/YvesBellenger/indus_project/ * 
+ */
 echo 'hello';
-
 ?>
