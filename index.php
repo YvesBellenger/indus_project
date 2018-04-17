c0a4d5c4806e2dd0e14e775205883e1a8087011f
Yves BELLENGER <yvesbellenger@hotmail.com>
2018-04-17 12:22:30 +0200

    test deploy with error

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * MyIndex
- * 
- * PHP Version 7.1
- *
- * @category Test
- * @package  MyApp
- * @author   Yves BELLENGER <yvesbellenger@hotmail.com>
- * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     https://github.com/YvesBellenger/indus_project/ * 
- */
-echo 'App deployed on Heroku'
+
+echo 'App deployed on Heroku';
 ?>
