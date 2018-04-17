3ee38d012a0b757b9724b8b1bb0c95463de99793
Yves BELLENGER <yvesbellenger@hotmail.com>
2018-04-17 12:25:29 +0200

    test deploy heroku

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-
+/**
+ * MyIndex
+ * 
+ * PHP Version 7.1
+ *
+ * @category Test
+ * @package  MyApp
+ * @author   Yves BELLENGER <yvesbellenger@hotmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/YvesBellenger/indus_project/ * 
+ */
 echo 'App deployed on Heroku';
 ?>
